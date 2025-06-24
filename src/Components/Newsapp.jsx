d81d478a8189aa579d353a2ed8f446215ad4451e
Kavya <edigakavya369@gmail.com>
2025-06-24 13:45:22 +0530

    Initial commit

---
+++ src/Components/Newsapp.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
+
+const Newsapp = () => {
+  const [search, setSearch]=useState("india");
+  const [newsData,setNewsData] = useState(null);
+  
+  const API_KEY= "3e5d3f90c40948aebfb271683a54a13e";
+
+
+
+  const getData=async()=>{
+       const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+       const jsonData=await response.json();
+       console.log(jsonData.articles);
+       setNewsData(jsonData.articles)
+  }
+  
+  useEffect(()=>{
+    getData()
+  },[])
+  const handleInput = (e)=>{
+    console.log(e.target.value);
+    setSearch(e.target.value)
+  }
+
+  const userInput = (event)=>{
+    setSearch(event.target.value)
+
+  }
+   
+  
+  return (
+    <div>
+      <nav>
+        <div>
+           <h1>Trendy News</h1> 
+        </div>
+        <ul>
+            <a href="">All News</a>
+            <a href="">Trending</a>
+        </ul>
+        <div className='searchBar'>
+            <input type="text" placeholder='Search News' value={search} onChange={handleInput}/>
+            <button onClick={getData}>Search</button>
+        </div>
+      </nav>
+      <div>
+        <p className='head'>Stay Update with Trendy News</p>
+      </div>
+
+      
+      <div className='categoryBtn'>
+        <button onClick={userInput} value="sports">Sports</button>
+        <button onClick={userInput} value="politics">Politics</button>
+        <button onClick={userInput} value="entertainment">Entertainment</button>
+        <button onClick={userInput} value="health">Health</button>
+        <button onClick={userInput} value="fitness">Fitness</button>
+
+      </div>
+      <div>
+        {newsData?<Card data={newsData}/>:null}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Newsapp
